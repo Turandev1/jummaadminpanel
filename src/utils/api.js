fc1a499c1f82be3b88f48776c91b7f6234936f0d
batur <baturturan2@gmail.com>
2026-01-25 18:23:59 +0400

    tgofnjrgto

--- src/utils/api.js
+++ src/utils/api.js
@@ -6,9 +6,9 @@
 
 //
 
-export const API_BASE_URL = "http://localhost:5000";
+// export const API_BASE_URL = "http://localhost:5000";
 
-// export const API_BASE_URL = "https://jummabackendapi.onrender.com";
+export const API_BASE_URL = "https://jummabackendapi.onrender.com";
 
 export const ADMIN_URL = `${API_BASE_URL}/webapi/admin`;
 
