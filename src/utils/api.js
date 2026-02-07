f86c81600a1efd623181273c92a8a73581b9da97
batur <baturturan2@gmail.com>
2026-02-07 09:39:57 +0400

    aegfn

--- src/utils/api.js
+++ src/utils/api.js
@@ -9,7 +9,6 @@
 export const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 
-console.log('API_BASE_URL',API_BASE_URL)
 export const ADMIN_URL = `${API_BASE_URL}/webapi/admin`;
 export const SATICI_URL = `${API_BASE_URL}/webapi/satici`;
 
